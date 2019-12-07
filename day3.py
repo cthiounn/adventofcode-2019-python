d8c6c084c140f7d76d83903988491afb2a432c89
Conrad THIOUNN <thiounn.conrad@gmail.com>
2019-12-07 19:59:42 +0100

    typo, keyword

--- day3.py
+++ day3.py
@@ -3,7 +3,7 @@ with open('data/my_input/day3-input.file',"r") as f:
 
 dx=dict(zip('LRUD',[-1,1,0,0]))
 dy=dict(zip('LRUD',[0,0,1,-1]))
-dict=[{},{}]
+mydict=[{},{}]
 
 def part1and2(num):
     i=0
@@ -19,12 +19,12 @@ def part1and2(num):
                 globalstep+=1
                 x+=dx[direction]
                 y+=dy[direction]
-                if (x,y) not in dict[i]:
-                    dict[i][(x,y)]=globalstep
+                if (x,y) not in mydict[i]:
+                    mydict[i][(x,y)]=globalstep
         i+=1
-    common=dict[0].keys() & dict[1].keys()
+    common=mydict[0].keys() & mydict[1].keys()
     minByDistIntersect=min(abs(x)+abs(y) for (x,y) in common)
-    minByStepIntersect=min(dict[0][point]+dict[1][point] for point in common)
+    minByStepIntersect=min(mydict[0][point]+mydict[1][point] for point in common)
     print(minByDistIntersect)
     print(minByStepIntersect)
 
