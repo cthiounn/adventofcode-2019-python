5e505a4964276a97b53eb3a4380f791efa585353
Conrad Thiounn <conrad.thiounn@insee.fr>
2019-12-10 09:20:08 +0100

    day9

--- day9.py
+++ day9.py
@@ -3,7 +3,7 @@ from collections import Counter
 import unittest
 
 
-with open('data/other_input/day9-input.file') as f:
+with open('data/my_input/day9-input.file') as f:
     numbers = [ line.strip() for line in f]
 
 def part1(st,input):
