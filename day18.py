9c47b65ee7a2ca17f2b9233d9a2d67d2fb73b0a2
Conrad Thiounn <conrad.thiounn@insee.fr>
2019-12-19 14:12:37 +0100

    day18 part two solved !

--- day18.py
+++ day18.py
@@ -8,9 +8,10 @@ with open("data/other_input/day18-input.file") as f:
 LRUD = {(0, 1), (0, -1), (1, 0), (-1, 0)}
 
 
-def part1(st):
+def part1(st, parttwo):
     d = dict()
     x, y = 0, 0
+    alldoors = dict()
     doors = dict()
     keys = dict()
     for j in range(len(st)):
@@ -22,13 +23,17 @@ def part1(st):
             elif line[i] in string.ascii_lowercase:
                 keys[(i, j)] = line[i]
             elif line[i] in string.ascii_uppercase:
-                doors[(i, j)] = line[i]
+                alldoors[(i, j)] = line[i]
+    for door in alldoors:  # filter for part 2
+        if alldoors[door].lower() in keys.values():
+            doors[door] = alldoors[door]
+        else:
+            d[door] = "."
     tiletoexplore = deque([])
     tiletoexplore.append((x, y, "", 0))
-    find = False
     kset = "".join(sorted(set(keys.values())))
     positions = set()
-    while len(tiletoexplore) != 0 and not find:
+    while len(tiletoexplore) != 0:
         tile = tiletoexplore.popleft()
         for lrud in LRUD:
             xt, yt, k, step = tile
@@ -60,16 +65,16 @@ def part1(st):
                 ks = "".join(sorted(k))
 
                 if ks == kset:
-                    print(step)
-                    find = True
-                    break
+                    if not parttwo:
+                        print(step)
+                    return step
                 if (nx, ny, ks) not in positions:
                     positions.add((nx, ny, ks))
                     tiletoexplore.append((nx, ny, k, step))
+    return 0
 
 
-# heavily inspired from u/jonathan_paulson
-def part2(st, to_init):
+def part2(st):
     d = dict()
     x, y = 0, 0
     doors = dict()
@@ -81,116 +86,53 @@ def part2(st, to_init):
             d[(i, j)] = line[i]
             if line[i] == "@":
                 x, y = i, j
-                if not to_init:
-                    robotos.append((x, y))
             elif line[i] in string.ascii_lowercase:
                 keys[(i, j)] = line[i]
             elif line[i] in string.ascii_uppercase:
                 doors[(i, j)] = line[i]
+    d[(x, y)] = "#"
+    d[(x + 1, y)] = "#"
+    d[(x - 1, y)] = "#"
+    d[(x, y + 1)] = "#"
+    d[(x, y - 1)] = "#"
+    d[(x + 1, y + 1)] = "@"
+    d[(x - 1, y - 1)] = "@"
+    d[(x - 1, y + 1)] = "@"
+    d[(x + 1, y - 1)] = "@"
 
-    if to_init:
-        d[(x, y)] = "#"
-        d[(x + 1, y)] = "#"
-        d[(x - 1, y)] = "#"
-        d[(x, y + 1)] = "#"
-        d[(x, y - 1)] = "#"
-        d[(x + 1, y + 1)] = "@"
-        d[(x - 1, y - 1)] = "@"
-        d[(x - 1, y + 1)] = "@"
-        d[(x + 1, y - 1)] = "@"
-        robotos.append((x - 1, y - 1))
-        robotos.append((x + 1, y - 1))
-        robotos.append((x - 1, y + 1))
-        robotos.append((x + 1, y + 1))
+    st1 = dicttolist(d, 0, x, 0, y)
+    st2 = dicttolist(d, x, len(st[0]), 0, y)
+    st3 = dicttolist(d, 0, x, y, len(st))
+    st4 = dicttolist(d, x, len(st[0]), y, len(st))
 
-    tiletoexplore = deque([])
-
-    posRobots = ";".join(map(repr, robotos))
-    tiletoexplore.append((posRobots, "", 0))
-    find = False
-    kset = "".join(sorted(set(keys.values())))
-    # positions = dict()
-    mindist = 9999999999999
-    while len(tiletoexplore) != 0 and not find:
-        tile = tiletoexplore.popleft()
-        posRobots, k, step = tile
-
-        robots = [eval(e) for e in posRobots.split(";")]
-        # posRobots = "".join(map(repr, robots))
-        # if (posRobots, k) in positions and step >= positions[(posRobots, k)]:
-        #     continue
-        # positions[(posRobots, k)] = step
-
-        # test invalidPosition
-        invalidPosition = False
-        for nrob in range(len(robotos)):
-            xr, yr = robots[nrob]
-            if (
-                d[(xr, yr)] == "#"
-                or (xr, yr) not in d
-                or (
-                    d[(xr, yr)] in string.ascii_uppercase
-                    and d[(xr, yr)].lower() not in k
-                )
-            ):
-                invalidPosition = True
-                break
-        if invalidPosition:
-            continue
-
-        # from a position, define what can any robot reach
-        # output a dictionnary of position: step, numofroboto
-        dpos = dict()
-        robotsmoves = deque([])
-        for numro in range(len(robotos)):
-            robotsmoves.append((robots[numro], numro, 0))
-        while len(robotsmoves) != 0:
-            pos, num, step = robotsmoves.popleft()
-            rx, ry = pos
-            if (
-                d[(rx, ry)] == "#"
-                or (rx, ry) not in d
-                or (
-                    d[(rx, ry)] in string.ascii_uppercase
-                    and d[(rx, ry)].lower() not in k
-                )
-                or pos in dpos
-            ):
-                continue
-            dpos[pos] = (step, num)
-            for lrud in LRUD:
-                dx, dy = lrud
-                nx, ny = rx + dx, ry + dy
-                if (nx, ny) not in d or d[(nx, ny)] == "#":
-                    continue
-                robotsmoves.append(((nx, ny), num, step + 1))
-
-        # from the uncatched-keys, evaluate from the dictionnary if a roboto can reach
-        # apply and submit new position
-        for key in keys:
-            if keys[key] not in k and key in dpos:
-                # inputs
-                dist, nro = dpos[key]
-                posRobots, k, step = tile
-
-                # calculate
-                dist += step
-                k += keys[key]
-
-                robots = [eval(e) for e in posRobots.split(";")]
-                robots[nro] = key
-                posRobots = ";".join(map(repr, robots))
-                # test if all keys are catched then output min
-                ks = "".join(sorted(k))
-                if ks == kset:
-                    if dist < mindist:
-                        mindist = dist
-
-                # submit next position
-                tiletoexplore.append((posRobots, k, dist))
-    print(mindist)
+    print(part1(st1, True) + part1(st2, True) + part1(st3, True) + part1(st4, True))
 
 
-# part1(numbers)
-part2(numbers, True)
+def printmap(d2):
+    xmin = min([x for x, y in d2])
+    ymin = min([y for x, y in d2])
+    xmax = max([x for x, y in d2])
+    ymax = max([y for x, y in d2])
+
+    # print(xmin, ymin, xmax, ymax)
+    for j in range(ymin - 1, ymax + 2):
+        sprint = ""
+        for i in range(xmin - 1, xmax + 2):
+            sprint += str(d2[(i, j)]) if (i, j) in d2 else "."
+        print(sprint)
+
+
+def dicttolist(d2, xmin, xmax, ymin, ymax):
+
+    li = []
+    for j in range(ymin, ymax + 1):
+        sprint = ""
+        for i in range(xmin, xmax + 1):
+            sprint += str(d2[(i, j)]) if (i, j) in d2 else "."
+        li.append(sprint)
+    return li
+
+
+part1(numbers, False)
+part2(numbers)
 
