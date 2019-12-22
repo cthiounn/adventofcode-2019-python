775d87957f427d5e0e827fab91f2624dba21dd14
Conrad THIOUNN <thiounn.conrad@gmail.com>
2019-12-22 08:32:31 +0100

    day22 part2 WIP

--- day22.py
+++ day22.py
@@ -2,6 +2,7 @@ import sys
 import string
 from collections import deque
 import re
+import math
 
 with open("data/other_input/day22-input.file") as f:
     numbers = [line.strip() for line in f]
@@ -55,7 +56,8 @@ def part2(st):
     st.reverse()
     j=2020
     #101741582076661
-    for _ in range(10):
+    for k in range(10000):
+        print(k-1,j)
         for s in st:
                 stack=re.findall("new stack",s)
                 increment=re.findall("increment",s)
@@ -63,25 +65,19 @@ def part2(st):
                 num=re.findall("-?\d+",s)
 
                 if len(stack)!=0:
-                    j=119315717514046-j
+                    j=119315717514047-1-j
                 elif len(cut)!=0:
                     # print("before",j)
                     index=int(num[0])
-                    if index>0:
-                        j=j+index
-                    else:
-                        j=j-index
+                    j=j+index+119315717514047
                     j%=119315717514047
                     # print(j,index)
                 elif(len(increment)!=0):
                     inc=int(num[0])
                     mod= j% inc
-                    if (mod==0):
-                        j= j/inc
-                    else:
-                        j=mod*inc +mod
-        print(j)
-    print(j)
+                    negativemod= mod-inc
+                    j=abs(mod)*inc + (j+abs(mod)) //inc 
+    
 
 
 part2(numbers)
