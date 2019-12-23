473d66150cb2823d74a84efaf37b2942fa2ad05a
Conrad Thiounn <conrad.thiounn@insee.fr>
2019-12-23 20:51:40 +0100

    day 23 part1

--- day23.py
+++ day23.py
@@ -1,8 +1,9 @@
-import string
 from collections import deque
+
 with open("data/other_input/day23-input.file") as f:
     numbers = [line.strip() for line in f]
 import asyncio
+
 
 def ra(a, d):
     return 0 if a not in d else d[a]
@@ -17,51 +18,45 @@ def part1(st):
     d = d2.copy()
     outputQueue = []
     inputQueues = [deque([]) for _ in range(256)]
+    status = [(0, 0, d.copy()) for _ in range(50)]
+    for j in range(len(inputQueues)):
+        inputQueues[j].append((j, -1))
     globalStop = False
-    total=0
-    x=0
-    y=0
-    dest=0
-    networkpackets=[]
-    for i in range(50):
-        # if len(networkpackets)==0:
-        result=computer(i,[],d)
-        x,y,dest =result
-        if (dest==255):
-            print(y)
-            # networkpackets.append(result )
-        inputQueues[dest].append((x,y))
-        # else:
-            # packets=[n for n in networkpackets if n[2]==i]
-            # result=asyncio.run(computer(i,[],d))
-    while True:
+    total = 0
+    x = 0
+    y = 0
+    dest = 0
+    stop = False
+    while not stop:
         for i in range(50):
-        # if len(networkpackets)==0:
-            print(i,inputQueues[i])
-            result=computer(i,inputQueues[i],d)
-            x,y,dest =result
-            if (dest==255):
+            iinit, rbinit, dinit = status[i]
+            oo = []
+            if len(inputQueues[i]) > 0:
+                oo.append(inputQueues[i].popleft())
+            result = computer(oo, iinit, rbinit, dinit)
+            x, y, iinit, rbinit, dinit, dest = result
+            status[i] = (iinit, rbinit, dinit)
+            if dest == 255:
                 print(y)
+                stop = True
                 break
-            elif dest==-1:
-                inputQueues[i].append((-1,-1))
+            # elif dest == -1:
+            #     inputQueues[i].append((-1, -1))
             else:
-                if (x,y) not in inputQueues[dest]:
-                    inputQueues[dest].append((x,y))
+                inputQueues[dest].append((x, y))
 
 
-def computer(input,net,d2):
-    x=0
-    y=0
-    dest=0
-    i = 0
-    rb = 0
+def computer(inputs, iinit, rbinit, d2):
+    x = 0
+    y = 0
+    dest = 0
+    i = iinit
+    rb = rbinit
     d = d2.copy()
     outputQueue = []
     inputQueue = deque([])
-    inputQueue.append(input)
-    if len(net)>0:
-        for n in net:
+    if len(inputs) > 0:
+        for n in inputs:
             inputQueue.append(n[0])
             inputQueue.append(n[1])
     globalStop = False
@@ -105,14 +100,15 @@ def computer(input,net,d2):
                 i += 4
             elif c == 3:
                 p1 = r2 + rb if pp1 == 2 else r2
-                if len(inputQueue)>0:
+
+                if len(inputQueue) > 0:
                     d[p1] = inputQueue.popleft()
+                    i += 2
                 else:
-                    return(-1,-1,-1)
+                    return (-1, -1, i, rb, d.copy(), -1)
                 # else:
-                    # d[p1] =-1
-                    # await asyncio.sleep(1)
-                i += 2
+                # d[p1] =-1
+                # await asyncio.sleep(1)
             elif c == 4:
                 outputQueue.append(p1)
                 i += 2
@@ -145,14 +141,12 @@ def computer(input,net,d2):
         if globalStop:
             break
         try:
-            y=outputQueue.pop()
-            x=outputQueue.pop()
-            dest=outputQueue.pop()
-            print(x,y,dest)
-            return (x,y,dest)
+            y = outputQueue.pop()
+            x = outputQueue.pop()
+            dest = outputQueue.pop()
+            return (x, y, i, rb, d.copy(), dest)
         except IndexError:
             break
 
-    
 
 part1(numbers[0])
